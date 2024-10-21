45dcf25621d9abd56a0afd852eee7b1743cdd18d
Ashwinigadad <ashwinigadad3@gmail.com>
2024-10-21 16:21:24 +0530

    server created

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-        <Button gradientDuoTone='purpleToBlue'>
+        <Button gradientDuoTone='purpleToBlue' outline>
           SignIn
         </Button>
         </Link>
@@ -64,3 +64,4 @@ export default function Header() {
     </Navbar>
   );
 }
+
