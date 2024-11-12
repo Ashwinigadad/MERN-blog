4ea1f2c3978f27b2ec595d1c4086d13a1010842f
Ashwinigadad <ashwinigadad3@gmail.com>
2024-11-12 21:45:42 +0530

    usermodel

--- api/index.js
+++ api/index.js
@@ -2,13 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env' });
-console.log(process.env.MONGO)
-mongoose.connect(process.env.MONGO)
+dotenv.config();
+mongoose.connect("mongodb+srv://tangevvacse:qwert2004@mern-blog.rpq6m.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog")
 .then(()=>{
     console.log("Mongodb is connected");
 })
-.catch(err=>{
+.catch((err)=>{
     console.log(err);
 })
 const app=express();
