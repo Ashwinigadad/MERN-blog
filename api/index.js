d2a20cd9999bf81ba8597b03d731cd1928c6baa4
Ashwinigadad <ashwinigadad3@gmail.com>
2024-11-13 10:25:02 +0530

    signup route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 mongoose.connect("mongodb+srv://tangevvacse:qwert2004@mern-blog.rpq6m.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog")
@@ -12,7 +13,7 @@ mongoose.connect("mongodb+srv://tangevvacse:qwert2004@mern-blog.rpq6m.mongodb.ne
     console.log(err);
 })
 const app=express();
-
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log(`server listening on port 3000`);
@@ -20,3 +21,4 @@ app.listen(3000,()=>{
 })
 
 app.use('/api',userRoutes);
+app.use('/auth',authRoutes);
